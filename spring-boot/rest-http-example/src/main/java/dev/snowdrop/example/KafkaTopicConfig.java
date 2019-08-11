a2784d05aa2c4eef102850399e96d9f7ff6e41f3
Lev Shulman <lshulman@redhat.com>
2019-08-10 22:45:59 -0600

    added kafka listener code

--- spring-boot/rest-http-example/src/main/java/dev/snowdrop/example/KafkaTopicConfig.java
+++ spring-boot/rest-http-example/src/main/java/dev/snowdrop/example/KafkaTopicConfig.java
@@ -5,16 +5,24 @@ import java.util.Map;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+@EnableKafka
 @Configuration
 public class KafkaTopicConfig {
     @Value(value = "${kafka.bootstrapAddress:my-cluster-kafka-bootstrap:9092}")
@@ -51,4 +59,39 @@ public class KafkaTopicConfig {
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+    
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+          ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+          bootstrapAddress);
+//        props.put(
+//          ConsumerConfig.GROUP_ID_CONFIG, 
+//          "group1");
+        props.put(
+          ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
+          StringDeserializer.class);
+        props.put(
+          ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
+          StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> 
+      kafkaListenerContainerFactory() {
+    
+        ConcurrentKafkaListenerContainerFactory<String, String> factory
+          = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+    
+    @KafkaListener(topics = "baeldung")
+    public void listen(String message) {
+        System.out.println("Received Messasge in group group1: " + message);
+    }
+    
+    
 }
